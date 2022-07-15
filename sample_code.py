7c8e7f0e1cc6aef23d8b6c7875182076275bfc83
peternguyen39 <nqktuyen19@apcs.fitus.edu.vn>
2022-07-15 21:00:43 +0700

    Finish task 6.1, 6.2, with 6.3 remaining

--- sample_code.py
+++ sample_code.py
@@ -4,9 +4,13 @@
 def xor(first, second):
    return bytearray(x^y for x,y in zip(first, second))
 
-MSG   = "A message"
-HEX_1 = "aabbccddeeff1122334455"
-HEX_2 = "1122334455778800aabbdd"
+# MSG   = "A message"
+# HEX_1 = "aabbccddeeff1122334455"
+# HEX_2 = "1122334455778800aabbdd"
+
+MSG = "This is a known message!"
+HEX_1 = "a469b1c502c1cab966965e50425438e1bb1b5f9037a4c159"
+HEX_2 = "bf73bcd3509299d566c35b5d450337e1bb175f903fafc159"
 
 # Convert ascii string to bytearray
 D1 = bytes(MSG, 'utf-8')
@@ -15,9 +19,13 @@ D1 = bytes(MSG, 'utf-8')
 D2 = bytearray.fromhex(HEX_1)
 D3 = bytearray.fromhex(HEX_2)
 
-r1 = xor(D1, D2)
-r2 = xor(D2, D3)
-r3 = xor(D2, D2)
+# r1 = xor(D1, D2)
+# r2 = xor(D2, D3)
+# r3 = xor(D2, D2)
+r1 = xor(D2, D3)
+r2 = xor(r1, D1)
+for i in range(0, len(r2)):
+    r2[i] = r2[i]^1
 print(r1.hex())
-print(r2.hex())
-print(r3.hex())
+print(r2.decode('utf-8'))
+# print(r3.hex())
